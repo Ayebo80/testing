52ac5358ad81cebf411d782233b46a9ac23c72ba
Ayebo80 <patrickmaurer@gmx.de>
2017-12-29 13:01:38 +0100

    node-testrail-api
    node-testrail-api

--- protractor/test/zoo_test_spec.js
+++ protractor/test/zoo_test_spec.js
@@ -6,6 +6,47 @@
 // 1) Maintaining becomes easier
 // 2) Easier to write & manage tests (duplication will reduce)
 
+// custom_reporter.js: https://jasmine.github.io/2.8/custom_reporter.html
+var myReporter = {
+
+  jasmineStarted: function(suiteInfo) {
+    console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
+  },
+
+  suiteStarted: function(result) {
+    console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
+  },
+
+  specStarted: function(result) {
+    console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
+  },
+
+  specDone: function(result) {
+    console.log('Spec: ' + result.description + ' was ' + result.status);
+    for(var i = 0; i < result.failedExpectations.length; i++) {
+      console.log('Failure: ' + result.failedExpectations[i].message);
+      console.log(result.failedExpectations[i].stack);
+    }
+	  console.log(result.passedExpectations.length);
+  },
+  
+  //suiteDone is invoked when all of the child specs and suites for a given suite have been run
+  suiteDone: function(result) {
+    console.log('Suite: ' + result.description + ' was ' + result.status);
+    for(var i = 0; i < result.failedExpectations.length; i++) {
+      console.log('AfterAll ' + result.failedExpectations[i].message);
+      console.log(result.failedExpectations[i].stack);
+    }
+  },
+  
+  jasmineDone: function() {
+    console.log('Finished suite');
+  }
+};
+
+jasmine.getEnv().addReporter(myReporter);
+
+
 var util = require ('util')
 
 describe ('To test the animal adoption flow', function() {
--- protractor/test/zoo_test_spec_testrail-api.js
+++ protractor/test/zoo_test_spec_testrail-api.js
@@ -7,54 +7,6 @@ var testrail = new Testrail({
 	user: 'ayebo@gmx.de',
 	password: 'ENYVQ3OeKTR24AmFPKLV-dZhWJt1X.sKLQl/QP.Ty'
 });
-	
-//status_id: 5: "failed", 1: "passed"	
-//		testrail.addResultsForCases(/*RUN_ID=*/1, /*CONTENT=*/{ "results": [{"case_id": 1, "status_id": 5, "comment": "this test has failed in protractor" }] }, function (err, results)
-//		{
-//			console.log("TC-1 Geschrieben!!!!");
-//		});
-	
-
-var myReporter = {
-
-  jasmineStarted: function(suiteInfo) {
-    console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
-  },
-
-  suiteStarted: function(result) {
-    console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
-  },
-
-  specStarted: function(result) {
-    console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
-  },
-
-  specDone: function(result) {
-    console.log('Spec: ' + result.description + ' was ' + result.status);
-    for(var i = 0; i < result.failedExpectations.length; i++) {
-      console.log('Failure: ' + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
-	  console.log(result.passedExpectations.length);
-  },
-  
-  //suiteDone is invoked when all of the child specs and suites for a given suite have been run
-  suiteDone: function(result) {
-    console.log('Suite: ' + result.description + ' was ' + result.status);
-    for(var i = 0; i < result.failedExpectations.length; i++) {
-      console.log('AfterAll ' + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
-  },
-  
-  jasmineDone: function() {
-    console.log('Finished suite');
-  }
-};
-
-jasmine.getEnv().addReporter(myReporter);
-
-
 
 describe ('To test the animal adoption flow', function() {
 	
@@ -82,29 +34,17 @@ describe ('To test the animal adoption flow', function() {
 	var home_page = require('../page/home_page.js');
 	
 	it ('Should be able to adopt an animal by page objects', function() {
-		home_page.enterFieldValue('DDDDDDDDYou will subscribe');
+		home_page.enterFieldValue('You will subscribe');
 		var getHomePageText = home_page.getDynamicText();
 		expect(getHomePageText).toBe('You will subscribe');
 		
-		if (getHomePageText == 'You will subscribe') {
-			console.log('you will subscribe');
-		}
-		
 		var animal_page = home_page.clickContinue();
-		console.log('Page 1: home_page done');
 		
 		animal_page.selectAnimal(2);
 		var confirm_page = animal_page.clickContinue();
-		console.log('Page 2: animal_page done');
-		
-		//no_error_occured = expect(confirm_page.getTitle()).toBe('Thank you');
-		console.log('Page 3: confirm_page done');
-		
-		//var v2 = home_page.AddNumbers(1,2);
-		//console.log(v2);
 		
 		//status_id: 5: "failed", 1: "passed"	
-		testrail.addResultsForCases(/*RUN_ID=*/1, /*CONTENT=*/{ "results": [{"case_id": 1, "status_id": 5, "comment": "für martin test" }] }, function (err, results)
+		testrail.addResultsForCases(/*RUN_ID=*/1, /*CONTENT=*/{ "results": [{"case_id": 1, "status_id": 5, "comment": "test failed" }] }, function (err, results)
 		{
 			console.log("TC-1 Geschrieben!!!!");
 		});
